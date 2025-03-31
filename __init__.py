04f5eab0f9ad4a68b13c9e8805a0322ebb874511
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-03-31 15:49:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,11 +3,6 @@ from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
 
-from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
-
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
@@ -17,9 +12,6 @@ def exercice(valeur):
             etoiles += '*'
         etoiles += '<br>'
     return etoiles
-
 if __name__ == "__main__":
   app.run(debug=True)
 
-if __name__ == "__main__":
-  app.run(debug=True)
