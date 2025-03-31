cba54e7cf34e2d98e399d797035ad23f536d55a4
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-03-31 12:44:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
+def exercice():
     etoiles = ''
-    for i in range(5):
-        etoiles += '*'
-    return etoiles
+    for i in range (5):
+        for j in range(5):
+            etoiles += '*'
+        return etoiles
 
 
 if __name__ == "__main__":
