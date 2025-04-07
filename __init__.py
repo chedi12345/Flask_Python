f6c58591c19fd02c63bfdc01975a06f794a19bd0
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-04-07 12:22:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,17 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route('/<int:n>')
+s=0
 def somme (n):
-    s=0
     for i in  range (n):
         if (i%5==0) or (i%7==0):
             s+=i
         elif (n%11==0):
             i+=1
-        if (s<=5000):
-            print(s)
-        else:
-            print(s-i)
+        if s> 5000:
+            return f"<h1>Somme dépassée : {s}</h1><p>Arrêt à i = {i}</p>"
+
+    return f"<h1>Somme finale : {s}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
