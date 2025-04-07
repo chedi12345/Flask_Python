189d1f28e4fb1d27f6fd61e7d2ff8b2a4c88ecfc
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-04-07 09:40:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,13 +3,17 @@ from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
 
-n=int(input("donner le nombre de base de pyramid"))
-k=""
-for i in range (1,n+1):
-    k=k+str(i)
-    l=""
-    for j in range(0,len(k-1),-1):
-        l=l+str(j)
+def generer_pyramide(valeur):
+    for i in range(1, valeur + 1):  # Pour chaque ligne de 1 à valeur
+        espaces = ' ' * (valeur - i)  # Ajoute des espaces pour centrer
+        partie_croissante = ''.join(str(j) for j in range(1, i + 1))  # Exemple : 123
+        partie_decroissante = ''.join(str(j) for j in range(i - 1, 0, -1))  # Exemple : 21
+        ligne = espaces + partie_croissante + partie_decroissante  # Combine tout
+        print(ligne)  # Affiche la ligne
+
+# Demande à l'utilisateur un nombre
+valeur_utilisateur = int(input("Entrez un nombre pour générer la pyramide : "))
+generer_pyramide(valeur_utilisateur))
     
 if __name__ == "__main__":
   app.run(debug=True)
