f4b2fc3522fcb29488487c7fa6bae7824c20ba9d
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-04-07 12:27:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,15 +8,13 @@ def calculer_somme(n):
 
     for i in range(1, n + 1):
         if i % 11 == 0:
-            continue  # On ignore les nombres divisibles par 11
+            i+=1
 
         if i % 5 == 0 or i % 7 == 0:
             somme += i
 
         if somme > 5000:
-            return f"<h1>Somme dépassée : {somme}</h1><p>Arrêt à i = {i}</p>"
-
-    return f"<h1>Somme finale : {somme}</h1>"
-
+            return(somme) 
+    return(somme) 
 if __name__ == "__main__":
     app.run(debug=True)
