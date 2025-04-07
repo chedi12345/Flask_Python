6b73dc982ef20603718f12782c4d6662300c2365
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-04-07 10:08:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,23 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def pyramide(valeur):
-    lignes = []
-    for i in range(1, valeur + 1):
-        espaces = ' ' * (valeur - i)
-        gauche = ''.join(str(j) for j in range(1, i + 1))
-        droite = ''.join(str(j) for j in range(i - 1, 0, -1))
-        ligne = espaces + gauche + droite
-        lignes.append(ligne)
+    resultat = '<pre>'
     
-    return render_template(lignes=lignes, valeur=valeur) 
-if name == "main":
+    for i in range(1, valeur + 1):
+        # Créer la partie avant le sommet
+        gauche = ''.join(str(x) for x in range(1, i + 1))
+        # Créer la partie après le sommet
+        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
+        
+        # Ajouter les espaces nécessaires pour centrer
+        espaces = ' ' * (valeur - i)
+        
+        # Combiner tout cela
+        resultat += espaces + gauche + droite + '\n'
+    
+    resultat += '</pre>'
+    return resultat
+
+ 
+if __name__ == "__main__":
     app.run(debug=True)
