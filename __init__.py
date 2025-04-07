fb2437b0bf67bca29a04cb242d95b08b1e1f4b86
chedi12345 <chedi.jaballi@ecoles-epsi.net>
2025-04-07 09:45:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+from flask import json
 
-def generer_pyramide(valeur):
-    for i in range(1, valeur + 1):  
-        espaces = ' ' * (valeur - i)  
-        partie_croissante = ''.join(str(j) for j in range(1, i + 1))  
-        partie_decroissante = ''.join(str(j) for j in range(i - 1, 0, -1))  
-        ligne = espaces + partie_croissante + partie_decroissante  
-        print(ligne)  # Affiche la ligne
+app = Flask(name)
 
-valeur_utilisateur = int(input("Entrez un nombre pour générer la pyramide : "))
-generer_pyramide(valeur_utilisateur))
-    
-if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/<int:valeur>')
+def pyramide(valeur):
+    resultat = '<pre>'
 
+    for i in range(1, valeur + 1):
+        # Créer la partie avant le sommet
+        gauche = ''.join(str(x) for x in range(1, i + 1))
+        # Créer la partie après le sommet
+        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
+
+Ajouter les espaces nécessaires pour centrer
+        espaces = ' ' * (valeur - i)
+
+Combiner tout cela
+        resultat += espaces + gauche + droite + '\n'
+
+    resultat += '</pre>'
+    return resultat
+
+ 
+if name == "main":
+    app.run(debug=True)
